1492c101526da5ae2f438e9e1a3394ccf64fa5ca
Sam-Dunne <cras.sdunne@gmail.com>
2021-02-10 08:54:53 -0600

    more finished

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var locationsArray = ['kitchen', 'bedroom', 'den', 'living room', 'pantry', 'clo
 var weaponsArray = ['knife', 'spoon', 'fork', 'spatula', 'toaster', 'brick', 'wiffle ball bat', 'drumstick', 'tea cup', 'battle axe', 'broomstick', 'universal remote', 'frying pan', 'tire iron', 'football', 'ping pong paddle', 'car keys', 'shovel', 'hamster', 'tennis racquet'];
 var friendsArray = ['Chase', 'Katie', 'Devonte', 'Ryan', 'Emil'];
 
-for (var i = 0; i < 100; i++) {
-    var newH3 = $("<h3>" + 'Accusation ' + (i+1) + "</h3>");
+for (var i = 1; i <= 100; i++) {
+    var newH3 = $("<h3>" + 'Accusation ' + i + "</h3>");
     $('body').append(newH3);
     
     function accuse() {
-        var accusation = i+1;
+        var accusation = i;
         var friend = friendsArray[i % 5];          // mod with max value 4
         var location = locationsArray[i % 10];     // mod with max value 9
         var weapon = weaponsArray[i % 20];         // mod with max value 19
